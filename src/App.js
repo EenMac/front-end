a84c50da98a2c230f3f9956f3d63aab07be0c46d
EenMacFullstack <Iain.mcdulling@gmail.com>
2022-07-13 21:41:21 +0100

    add changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { TextField, Button } from '@mui/material';
+import { useState } from 'react';
 
 function App() {
+  const [student, setStudent] = useState('Iain');
+  const [address, setAddress] = useState('Govan');
+
+  const handleClick = (event) => {
+    const identity = {student, address}
+    console.log(identity)
+    fetch("http://localhost:8080/employee/add", {
+      method: "POST",
+      headers: {"Content-Type":"application/json"},
+      body: JSON.stringify(student),
+    }).then(() => {
+    console.log("New student Added")
+  });
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div>Hello</div>
+        <div className='form'>
+          <TextField
+            value={student}
+            id="outlined-name"
+            label="Name"
+            onChange={(event) => setStudent(event.target.value)}
+          />
+          <TextField
+            value={address}
+            id="outlined-uncontrolled"
+            label="Address"
+            onChange={(event) => setAddress(event.target.value)}
+          />
+        </div>
+        <Button label="Button" variant="contained" onClick={handleClick} >Click me</Button>
+      </div>
+      {student}
+      {address}
     </div>
   );
 }
